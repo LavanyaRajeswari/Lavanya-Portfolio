b7b8e6a07af35303442f4ef8d26a7ae15e2751ec
Lavanya Rajeswari <saridelavanyarajeswari@gmail.com>
2025-02-20 11:23:25 +0530

    Initial Commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,13 @@
+export const personalData = {
+  name: "Lavanya Rajeswari",
+  profile: '/profile2.png',
+  designation: "Software Developer",
+  description: "My name is Lavanya Rajeswari. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'saridelavanyarajeswari@gmail.com',
+  phone: '+919959327498',
+  address: 'Rajahmundry, Andhra Pradesh, India',
+  github: 'https://github.com/LavanyaRajeswari',
+  linkedIn: 'https://www.linkedin.com/in/lavanya-rajeswari-saride-7b856a288/',
+  leetcode: "https://leetcode.com/u/Saride_Lavanya_Rajeswari/",
+  resume: "https://drive.google.com/file/d/1-_PT4hS7wcjpVyc1mDLzh0MPRjTKj_rn/view?usp=sharing"
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,34 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'Live Bus',
+        description: "Me and my team built a GPS-based bus tracking system that delivers real-time location updates and route information, improving operational efficiency and enhancing user experience. An intuitive user interface enabling students to track college buses and view real-time estimated arrival times, enhancing user convenience and operational efficiency.",
+        tools: ['Express', 'MongoDB', 'React'],
+        role: 'Full Stack Developer',
+        // code: '',
+        // demo: '',
+    },
+    {
+        id: 2,
+        name: 'Doc Deal',
+        description: 'We have Developed a web application to streamline doctor appointment booking, enhancing accessibility and convenience for patients. Designed and implemented features for scheduling, managing, and canceling appointments, with functionality tailored to doctors’ specializations.',
+        tools: ['HTML', 'CSS', 'JavaScript'],
+        role: 'Full Stack Developer',
+        // code: '',
+        // demo: '',
+    }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
